6949602a0c42e4fffe8542a3a74a3e0ed1c709c7
Nate Sheridan <83609521+natesheridan@users.noreply.github.com>
2021-09-22 22:49:48 -0600

    Initial commit

---
+++ test/Customer-test.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+import Customer from '../src/js/user/Customer';
+
+const expect = chai.expect;
+
+describe('See if the tests are running', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+});
